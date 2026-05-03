fc7e49c5496bbc29377c7d7a68697a07b2e89124
Hudson Shields <hudsonshields@login9.ufhpc>
2026-05-03 01:08:23 -0400

    resolve merge conflict

--- shell_scripts/multi_cand.sh
+++ shell_scripts/multi_cand.sh
@@ -3,15 +3,19 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
-#SBATCH --gpus=5
-#SBATCH --partition=gpu
+#SBATCH --gpus=3
+#SBATCH --partition=hpg-turin
 #SBATCH --mem=64gb
 
-#SBATCH --time=00:05:00
+#SBATCH --time=00:30:00
 
 #SBATCH --job-name=multi_cand_eval
 #SBATCH --output=hpglogs/multi_cand_eval_%j.out
 #SBATCH --error=hpglogs/multi_cand_eval_%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=hudsonshields@ufl.edu
+
+cd /blue/chunmei.wang/hudsonshields/GraphFEX
 
 module load conda
 conda activate eccentric_env
@@ -21,4 +25,4 @@ echo "Host: $(hostname)"
 echo "CPUs: $SLURM_CPUS_PER_TASK"
 echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
 
-python /HR/eval_scripts/multi_cand_eval.py
+python -u HR/scripts/eval_controller.py
